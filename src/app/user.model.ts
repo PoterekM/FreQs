498da011275df37973745ab66d0ab762c4a364bd
Michelle Poterek <poterekm@gmail.com>
2017-08-25 09:44:27 -0700

    create form to add a new user

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,3 +1,3 @@
 export class User {
-  constructor (public name: string, public title: string, public description: string, public image: string) { }
+  constructor (public name: string, public title: string, public description: string, public image: string, public website: string) { }
 }
